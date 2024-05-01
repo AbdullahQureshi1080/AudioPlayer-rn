5ca8bcd2631099761f93de8bb3b482b6ccab2fde
Abdullah Qureshi <adnx1080@gmail.com>
2024-05-01 17:10:59 +0500

    feat: player-example added complete

--- service.js
+++
@@ -1,8 +0,0 @@
-// service.js
-import { Event } from 'react-native-track-player';
-
-export const PlaybackService = async function () {
-    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
-    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
-
-};
